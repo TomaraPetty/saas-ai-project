8d94a56bcd808e5d4243c25e82aebfdc41d4cdb2
Tomara Petty <tomararuth@gmail.com>
2024-03-17 16:35:13 -0700

    add image and email to be displayed when user is logged in

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
 export default function Home() {
@@ -9,7 +10,22 @@ export default function Home() {
     <div>
       <h1>This is the homepage</h1>
       <div>
-        <Link href='/api/auth/login'>Login</Link>
+        {!!user ? (
+          <>
+            <div>
+              <Image
+                src={user.picture}
+                alt={user.name}
+                height={50}
+                width={50}
+              />
+              <div>{user.email}</div>
+            </div>
+            <Link href='/api/auth/logout'>Logout</Link>
+          </>
+        ) : (
+          <Link href='/api/auth/login'>Login</Link>
+        )}
       </div>
     </div>
   );
