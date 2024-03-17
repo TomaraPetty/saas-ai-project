ab000804b5921820a528e4e794383a78083db2ae
Tomara Petty <tomararuth@gmail.com>
2024-03-17 16:07:21 -0700

    add link to auth0 login callback and console log user info

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  console.log('error:', error, isLoading, user);
   return (
     <div>
-      <h1>This is the home page</h1>
+      <h1>This is the homepage</h1>
+      <div>
+        <Link href='/api/auth/login'>Login</Link>
+      </div>
     </div>
   );
 }
