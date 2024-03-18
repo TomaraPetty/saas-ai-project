d87642f7f1dc17947ffb6ce7f56373354be23437
Tomara Petty <tomararuth@gmail.com>
2024-03-18 15:04:54 -0700

    add styling to the side bar content

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,8 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { useUser } from '@auth0/nextjs-auth0/client';
-
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-
-  console.log('error:', error, isLoading, user);
   return (
     <div>
       <h1>This is the homepage</h1>
-      <div>
-        {!!user ? (
-          <>
-            <div>
-              <Image
-                src={user.picture}
-                alt={user.name}
-                height={50}
-                width={50}
-              />
-              <div>{user.email}</div>
-            </div>
-            <Link href='/api/auth/logout'>Logout</Link>
-          </>
-        ) : (
-          <Link href='/api/auth/login'>Login</Link>
-        )}
-      </div>
+      <div></div>
     </div>
   );
 }
