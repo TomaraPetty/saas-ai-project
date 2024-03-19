d9190ad5a2ad02c95aee3c9be55d9be4b521d3f4
Tomara Petty <tomararuth@gmail.com>
2024-03-18 18:22:37 -0700

    add title and metadata

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -33,7 +33,37 @@ export default async function handler(req, res) {
     ],
   });
 
-  res
-    .status(200)
-    .json({ postContent: response.data.choices[0]?.message?.content });
+  const postContent = response.data.choices[0]?.message?.content;
+  const seoResponse = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo-1106',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You are an SEO friendly blog post generator called BlogStandard. You are designed to output JSON. Do not include HTML tags in your output.',
+      },
+      {
+        role: 'user',
+        content: `Generate an SEO friendly title and SEO friendly meta description for the following blog post: ${postContent}
+        ---
+        The output json must be in the following format:
+        { 
+          "title": "example title",
+          "metaDescription": "example meta description",
+        }`,
+      },
+    ],
+    response_format: { type: 'json_object' },
+  });
+
+  const { title, metaDescription } =
+    seoResponse.data.choices[0]?.message?.content || {};
+
+  res.status(200).json({
+    post: {
+      postContent,
+      title,
+      metaDescription,
+    },
+  });
 }
