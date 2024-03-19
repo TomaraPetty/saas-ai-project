7b1ae8dff7ef8fbad72ce8464ed855e9eb34c0c5
Tomara Petty <tomararuth@gmail.com>
2024-03-18 18:39:00 -0700

    add submit form to make api call and send topic and keywords as variables

--- pages/post/new.js
+++ pages/post/new.js
@@ -5,10 +5,17 @@ import Markdown from 'react-markdown';
 
 export default function NewPost(props) {
   const [postContent, setPostContent] = useState('');
+  const [topic, setTopic] = useState('');
+  const [keywords, setKeywords] = useState('');
   console.log('new post props', props);
-  const handleSubmit = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const response = await fetch(`/api/generatePost`, {
       method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ topic, keywords }),
     });
     const json = await response.json();
     console.log('RESULT:', json);
@@ -17,10 +24,31 @@ export default function NewPost(props) {
 
   return (
     <div>
-      <h1>This is the new post page</h1>
-      <button className='btn' onClick={handleSubmit}>
-        Generate
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on the topic of:</strong>
+          </label>
+          <textarea
+            className='resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm'
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          ></textarea>
+        </div>
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            className='resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm'
+            value={keywords}
+            onChange={(e) => setKeywords(e.target.value)}
+          ></textarea>
+        </div>
+        <button className='btn' type='submit'>
+          Generate
+        </button>
+      </form>
       <Markdown>{postContent}</Markdown>
     </div>
   );
