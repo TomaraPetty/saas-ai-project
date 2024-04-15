b149171eab2354eace7856f385a3a05e8e784b9d
Tomara Petty <tomararuth@gmail.com>
2024-04-15 15:42:28 -0700

    change logo icon to phone

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,13 +1,13 @@
-import { faBrain } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Logo = () => {
   return (
     <div className='text-3xl text-center py-4 font-heading'>
-      BlogStandard
+      Make Noise
       <FontAwesomeIcon
-        icon={faBrain}
-        className='pl-2 text-2xl text-slate-400'
+        icon={faPhone}
+        className='pl-2 text-2xl text-slate-400 m-2'
       />
     </div>
   );
